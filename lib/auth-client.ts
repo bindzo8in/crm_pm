23afb7e0b3cb5e940d444483459228234171406d
oneclicktechy <oneclicktechy@gmail.com>
2026-07-04 14:26:54 +0530

    feat: initialize better-auth client and configure environment variable validation schema

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,10 +2,11 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 import { ac, adminRole, staffRole, superAdminRole } from "@/lib/permissions";
 import { UserRole } from "@/app/generated/prisma/enums";
+import { env } from "./env";
 
 export const { signIn, signUp, useSession, signOut, requestPasswordReset, resetPassword, sendVerificationEmail, admin } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: env.NEXT_PUBLIC_SITE_URL,
     plugins: [
         adminClient({
             ac,
